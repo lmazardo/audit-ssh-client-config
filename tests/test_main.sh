f5499fe6a1a7dbb68bd6d38bf35bc25cd7d46b89
Pamplemousse <xav.maso@gmail.com>
2017-06-22 13:10:40 +0200

    Message showing multiple time corrected
      * do not loop through dict keys (which are generated by concatenating:
      *eg*: `*PasswordAuthentication`, `*ChallengeResponseAuthentication`
      * use a list of hosts instead
      * `next` statement seems rather useless

--- tests/test_main.sh
+++ tests/test_main.sh
@@ -4,6 +4,21 @@ source ./test_password_authentication
 source ./test_challenge_response_authentication
 source ./test_kex_algorithms
 
+
+test_each_message_is_displayed_only_once() {
+  cat << __EOF__ >"fake_bad_config"
+Host *
+__EOF__
+
+  ../main fake_bad_config > result
+
+  # PasswordAuthentication, ChallengeResponseAuthentication,
+  # KexAlgorithms
+  # are missing and therefore trigger an error
+  assert_equals 3 "$(grep -c ERROR result)"
+}
+
+
 teardown() {
   rm fake*
   rm result*
