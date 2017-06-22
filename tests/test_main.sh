43c762a5007d33a6c25eea8feb322cd2d9cc9c28
Pamplemousse <xav.maso@gmail.com>
2017-06-22 15:15:11 +0200

    Validate Message Authentication Codes

--- tests/test_main.sh
+++ tests/test_main.sh
@@ -6,6 +6,7 @@ source ./test_host_key_algorithms
 source ./test_kex_algorithms
 source ./test_ciphers
 source ./test_pubkey_authentication
+source ./test_macs
 
 
 test_each_message_is_displayed_only_once() {
@@ -16,9 +17,9 @@ __EOF__
   ../main fake_bad_config > result
 
   # PasswordAuthentication, ChallengeResponseAuthentication, HostKeyAlgorithms
-  # KexAlgorithms, Ciphers, PubkeyAuthentication
+  # KexAlgorithms, Ciphers, PubkeyAuthentication, MACs
   # are missing and therefore trigger an error
-  assert_equals 6 "$(grep -c ERROR result)"
+  assert_equals 7 "$(grep -c ERROR result)"
 }
 
 
