6985cf850d407fc0981c526c7898217ce093c8e6
Pamplemousse <xav.maso@gmail.com>
2017-07-11 09:45:14 +0200

    Improve tests "setup"
      * use constants rather than setup: nothing is done here, this is just
      setting some values
      * use temp files whenever possible

--- tests/lib_test.sh
+++ tests/lib_test.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 TESTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-setup() {
-  MAIN="$TESTS_DIR/../audit-ssh-config.sh"
-  FAKE_BAD_CONFIG="$TESTS_DIR/fake_bad_config"
-  FAKE_BAD_CONFIG_2="$TESTS_DIR/fake_bad_config_2"
-  GOOD_STAR_HOST_CONFIG="$TESTS_DIR/fixtures/good_star_host_config"
-  RAW_GOOD_STAR_HOST_CONFIG="$TESTS_DIR/fixtures/raw_good_star_host_config"
-  RESULT="$TESTS_DIR/result"
-}
+MAIN="$TESTS_DIR/../audit-ssh-config.sh"
+
+GOOD_STAR_HOST_CONFIG="$TESTS_DIR/fixtures/good_star_host_config"
+RAW_GOOD_STAR_HOST_CONFIG="$TESTS_DIR/fixtures/raw_good_star_host_config"
+
+FAKE_BAD_CONFIG=$(mktemp)
+FAKE_BAD_CONFIG_2=$(mktemp)
+
+RESULT=$(mktemp)
+
 
 teardown() {
   rm "${FAKE_BAD_CONFIG}" &> /dev/null
